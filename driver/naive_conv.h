cf2216adf23faa33303d503d4b59c92048f8908b
carlushuang <carlus.huang@amd.com>
2020-05-26 10:52:05 +0000

    threaded cpu conv

--- driver/naive_conv.h
+++ driver/naive_conv.h
@@ -25,6 +25,51 @@
  *******************************************************************************/
 #ifndef __NAIVE_CONV_H
 #define __NAIVE_CONV_H
+
+#define NAIVE_CONV_THREADED
+
+#ifdef NAIVE_CONV_THREADED
+#include <thread>
+#include <vector>
+#include <functional>
+using naive_conv_threadwise_conv_t = std::function<void(size_t,size_t,size_t,size_t)>;
+
+class naive_conv_blockwise_4d_t{
+public:
+    naive_conv_blockwise_4d_t(naive_conv_threadwise_conv_t f):mf(f){}
+    void operator()(size_t thread_id, size_t block_size,
+        size_t d0, size_t d1, size_t d2, size_t d3)
+    {
+        size_t total_length = d0 * d1 * d2 * d3;
+        for(size_t tid = thread_id; tid<total_length ; tid += block_size){
+            size_t id0, id1, id2, id3;
+            get_index_4d(tid, d0, d1, d2, d3, &id0, &id1, &id2, &id3);
+            mf(id0, id1, id2, id3);
+        }
+    }
+private:
+    naive_conv_threadwise_conv_t mf;
+    void get_index_4d(size_t idx, size_t d0, size_t d1, size_t d2, size_t d3,
+                        size_t *id0, size_t *id1, size_t *id2, size_t *id3)
+    {
+        *id3 = idx % d3;
+        *id2 = (idx / d3) % d2;
+        *id1 = (idx / (d2*d3)) % d1;
+        *id0 = idx / (d1*d2*d3);        // TODO ignore final mod
+        (void)d0;
+    }
+};
+
+static inline void naive_conv_blockwise_in_parallel(naive_conv_threadwise_conv_t f, size_t d0, size_t d1, size_t d2, size_t d3)
+{
+    int num_threads = std::thread::hardware_concurrency();
+    std::vector<std::thread> threads(num_threads);
+    for(size_t tid = 0; tid < num_threads; tid++)
+        threads[tid] = std::thread(naive_conv_blockwise_4d_t(f), tid, num_threads, d0, d1, d2, d3);
+    for(size_t tid = 0; tid < num_threads; tid++)
+        threads[tid].join();
+}
+#endif
 static inline size_t naive_conv_out_size(size_t in_size, size_t pad,
                                          size_t dilation, size_t ksize,
                                          size_t stride) {
@@ -35,10 +80,36 @@ static inline void naive_conv_fwd_nchw(const float *src, const float *filter,
                                        size_t c, size_t k, size_t fx, size_t fy,
                                        size_t px, size_t py, size_t sx,
                                        size_t sy, size_t dx, size_t dy) {
-    size_t in, ik, ioh, iow, ic, is, ir;
-    size_t cur_h, cur_w, o_idx, i_idx, f_idx;
     size_t oh = naive_conv_out_size(h, py, dy, fy, sy);
     size_t ow = naive_conv_out_size(w, px, dx, fx, sx);
+#ifdef NAIVE_CONV_THREADED
+    auto conv_one_pixel = [&](size_t in, size_t ik, size_t ioh, size_t iow){
+        size_t ic, is, ir, cur_h, cur_w, o_idx, i_idx, f_idx;
+        float value = .0f;
+        o_idx = in * k * oh * ow + ik * oh * ow + ioh * ow + iow;
+        for (ic = 0; ic < c; ic++) {
+            for (ir = 0; ir < fy; ir++) {
+                cur_h = sy * ioh - py + dy * ir;
+                if (cur_h < 0 || cur_h >= h)
+                    continue;
+                for (is = 0; is < fx; is++) {
+                    cur_w = sx * iow - px + dx * is;
+                    if (cur_w < 0 || cur_w >= w)
+                        continue;
+                    i_idx = in * c * h * w + ic * h * w +
+                            cur_h * w + cur_w;
+                    f_idx = ik * c * fy * fx + ic * fy * fx +
+                            ir * fx + is;
+                    value += src[i_idx] * filter[f_idx];
+                }
+            }
+        }
+        dst[o_idx] = value;
+    };
+    naive_conv_blockwise_in_parallel(conv_one_pixel, n, k, oh, ow);
+#else
+    size_t in, ik, ioh, iow, ic, is, ir;
+    size_t cur_h, cur_w, o_idx, i_idx, f_idx;
     for (in = 0; in < n; in++) {
         for (ik = 0; ik < k; ik++) {
             for (ioh = 0; ioh < oh; ioh++) {
@@ -68,6 +139,7 @@ static inline void naive_conv_fwd_nchw(const float *src, const float *filter,
             }
         }
     }
+#endif
 }
 static inline void naive_conv_fwd_cnhw(const float *src, const float *filter,
                                        float *dst, size_t n, size_t w, size_t h,
