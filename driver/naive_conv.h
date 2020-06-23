979df0d32fb2ae8da052fdd8644577b6c010bc4d
carlushuang <carlus.huang@amd.com>
2020-06-23 07:54:21 +0000

    threaded bwd/wrw

--- driver/naive_conv.h
+++ driver/naive_conv.h
@@ -187,10 +187,48 @@ static inline void naive_conv_bwd_d_nchw(float *src_grad, const float *filter,
                                          size_t fx, size_t fy, size_t px,
                                          size_t py, size_t sx, size_t sy,
                                          size_t dx, size_t dy) {
-    size_t in, ik, ih, iw, ic, is, ir;
-    size_t cur_oh, cur_ow, o_idx, i_idx, f_idx;
     size_t oh = naive_conv_out_size(h, py, dy, fy, sy);
     size_t ow = naive_conv_out_size(w, px, dx, fx, sx);
+#ifdef NAIVE_CONV_THREADED
+    auto conv_one_pixel = [&](size_t in, size_t ic, size_t ih, size_t iw){
+        size_t ik, is, ir;
+        size_t cur_oh, cur_ow, o_idx, i_idx, f_idx;
+        float value = .0f;
+        i_idx = in * c * h * w + ic * h * w + ih * w + iw;
+        for (ik = 0; ik < k; ik++) {
+            for (ir = 0; ir < fy; ir++) {
+                cur_oh =
+                    ih + py - dy * ir; // cur_h = sy*ioh-py+dy*ir;
+                if (cur_oh < 0 || cur_oh % sy)
+                    continue;
+                cur_oh /= sy;
+                if (cur_oh >= oh)
+                    continue;
+                for (is = 0; is < fx; is++) {
+                    cur_ow = iw + px -
+                                dx * is; // cur_w = sx*iow-px+dx*is;
+                    if (cur_ow < 0 || cur_ow % sx)
+                        continue;
+                    cur_ow /= sx;
+                    if (cur_ow >= ow)
+                        continue;
+
+                    o_idx = in * k * oh * ow + ik * oh * ow +
+                            cur_oh * ow + cur_ow;
+                    f_idx = ik * c * fy * fx + ic * fy * fx +
+                            ir * fx + is;
+
+                    value += dst_grad[o_idx] * filter[f_idx];
+                }
+            }
+        }
+        src_grad[i_idx] = value;
+    };
+    naive_conv_blockwise_in_parallel(conv_one_pixel, n, c, h, w);
+#else
+    size_t in, ik, ih, iw, ic, is, ir;
+    size_t cur_oh, cur_ow, o_idx, i_idx, f_idx;
+
     for (in = 0; in < n; in++) {
         for (ic = 0; ic < c; ic++) {
             for (ih = 0; ih < h; ih++) {
@@ -229,6 +267,7 @@ static inline void naive_conv_bwd_d_nchw(float *src_grad, const float *filter,
             }
         }
     }
+#endif
 }
 static inline void naive_conv_bwd_d_cnhw(float *src_grad, const float *filter,
                                          const float *dst_grad, size_t n,
@@ -286,10 +325,38 @@ static inline void naive_conv_bwd_f_nchw(const float *src, float *filter_grad,
                                          size_t fx, size_t fy, size_t px,
                                          size_t py, size_t sx, size_t sy,
                                          size_t dx, size_t dy) {
-    size_t in, ik, ioh, iow, ic, is, ir;
-    size_t cur_h, cur_w, o_idx, i_idx, f_idx;
     size_t oh = naive_conv_out_size(h, py, dy, fy, sy);
     size_t ow = naive_conv_out_size(w, px, dx, fx, sx);
+#ifdef NAIVE_CONV_THREADED
+    auto conv_one_pixel = [&](size_t ik, size_t ic, size_t ir, size_t is){
+        size_t in, ioh, iow;
+        size_t cur_h, cur_w, o_idx, i_idx, f_idx;
+        float value = .0f;
+        f_idx = ik * c * fy * fx + ic * fy * fx + ir * fx + is;
+        for (in = 0; in < n; in++) {
+            for (ioh = 0; ioh < oh; ioh++) {
+                cur_h = sy * ioh - py + dy * ir;
+                if (cur_h < 0 || cur_h >= h)
+                    continue;
+                for (iow = 0; iow < ow; iow++) {
+                    cur_w = sx * iow - px + dx * is;
+                    if (cur_w < 0 || cur_w >= w)
+                        continue;
+                    i_idx = in * c * h * w + ic * h * w +
+                            cur_h * w + cur_w;
+                    o_idx = in * k * oh * ow + ik * oh * ow +
+                            ioh * ow + iow;
+                    value += src[i_idx] * dst_grad[o_idx];
+                }
+            }
+        }
+        filter_grad[f_idx] = value;
+    };
+    naive_conv_blockwise_in_parallel(conv_one_pixel, k, c, fy, fx);
+#else
+    size_t in, ik, ioh, iow, ic, is, ir;
+    size_t cur_h, cur_w, o_idx, i_idx, f_idx;
+
     for (ik = 0; ik < k; ik++) {
         for (ic = 0; ic < c; ic++) {
             for (ir = 0; ir < fy; ir++) {
@@ -318,6 +385,7 @@ static inline void naive_conv_bwd_f_nchw(const float *src, float *filter_grad,
             }
         }
     }
+#endif
 }
 
 static inline void naive_conv_bwd_f_cnhw(const float *src, float *filter_grad,
