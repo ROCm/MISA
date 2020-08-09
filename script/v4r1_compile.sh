b4e3c9121b227ce9d416dba25e76697e3d85678c
shaojiewang <wsjmessi@163.com>
2020-08-09 16:46:26 +0800

    add xdlops asm file

--- script/v4r1_compile.sh
+++ script/v4r1_compile.sh
@@ -14,14 +14,15 @@ driver/conv_driver.cpp -L/opt/rocm/hcc/lib -L/opt/rocm/lib \
 #-DIGEMM_HSACO="igemm_v4r1_dynamic.hsaco" \
 
 # device compilation
-/opt/rocm-3.5.0/llvm/bin/clang++ -x assembler -target amdgcn--amdhsa -mcpu=gfx906 out/igemm_v4r1_dynamic.s -o out/igemm_v4r1_dynamic.hsaco
+arch=gfx908
+/opt/rocm-3.5.0/llvm/bin/clang++ -x assembler -target amdgcn--amdhsa -mcpu=$arch out/igemm_v4r1_dynamic_xdlops.s -o out/igemm_v4r1_dynamic.hsaco
 
-/opt/rocm-3.5.0/llvm/bin/clang++  -std=c++14 -mcpu=gfx906 -Werror -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic \
+/opt/rocm-3.5.0/llvm/bin/clang++  -std=c++14 -mcpu=$arch -Werror -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic \
 -Wno-conversion -Wno-double-promotion -Wno-exit-time-destructors -Wno-extra-semi -Wno-float-conversion \
 -Wno-gnu-anonymous-struct -Wno-gnu-zero-variadic-macro-arguments -Wno-missing-prototypes -Wno-nested-anon-types \
 -Wno-padded -Wno-return-std-move-in-c++11 -Wno-shorten-64-to-32 -Wno-sign-conversion -Wno-unknown-warning-option \
 -Wno-unused-command-line-argument -Wno-weak-vtables -Wno-covered-switch-default -Wno-disabled-macro-expansion \
--Wno-undefined-reinterpret-cast --cuda-gpu-arch=gfx906 --cuda-device-only -c -O3  -Wno-unused-command-line-argument \
+-Wno-undefined-reinterpret-cast --cuda-gpu-arch=$arch --cuda-device-only -c -O3  -Wno-unused-command-line-argument \
 -I. -x hip --hip-device-lib-path=/opt/rocm/lib -mllvm -amdgpu-early-inline-all=true -mllvm \
 -amdgpu-function-calls=false -D__HIP_ROCclr__=1 -isystem /opt/rocm-3.5.0/hip/../include \
 -isystem /opt/rocm/llvm/lib/clang/11.0.0/include/.. -D__HIP_PLATFORM_HCC__=1 -D__HIP_ROCclr__=1 \
@@ -29,5 +30,5 @@ driver/conv_driver.cpp -L/opt/rocm/hcc/lib -L/opt/rocm/lib \
 --hip-link -mllvm -amdgpu-enable-global-sgpr-addr -mllvm --amdgpu-spill-vgpr-to-agpr=0 \
 out/wrw_reduction_hip.hip.cc -o out/wrw_reduction_hip.hip.cc.o -Wno-old-style-cast -Wno-cast-align
 
-/opt/rocm-3.5.0/llvm/bin/clang-offload-bundler --type=o --targets=hip-amdgcn-amd-amdhsa-gfx906 \
+/opt/rocm-3.5.0/llvm/bin/clang-offload-bundler --type=o --targets=hip-amdgcn-amd-amdhsa-$arch \
 --inputs=out/wrw_reduction_hip.hip.cc.o --outputs=out/wrw_reduction_hip.hip.cc.o.hsaco --unbundle
