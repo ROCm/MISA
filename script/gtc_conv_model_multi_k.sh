53b6db01eb03ed926ac47b4c6d9478a3b52346f3
shaojiewang <shaojie.wang@amd.com>
2021-01-27 12:29:25 +0800

    do not change unrelated files

--- script/gtc_conv_model_multi_k.sh
+++ script/gtc_conv_model_multi_k.sh
@@ -27,13 +27,9 @@ else
     exit 1
 fi
 
-./out/conv_driver.exe conv -n 16 -c 32 -H 8 -W 8 -k 4 -y 1 -x 2 -p 0 -q 0 -u 1 -v 1 -l 1 -j 1  -F $FORW
-#./out/conv_driver.exe conv -n 4 -c 32 -H 33 -W 129 -k 4 -y 2 -x 2 -p 0 -q 0 -u 1 -v 1 -l 1 -j 1 -g 1 -F $DIR -t 1 -w 1
-#./out/conv_driver.exe conv -n 4 -c 32 -H 79 -W 341 -k 4 -y 5 -x 10 -p 0 -q 0 -u 2 -v 2 -l 1 -j 1 -g 1 -F $DIR -t 1 -w 1
-
-#./out/conv_driver.exe conv -n 128 -c 128 -H 17 -W 17 -k 128 -y 1 -x 7 -p 0 -q 3 -u 1 -v 1 -l 1 -j 1  -F $FORW
-#./out/conv_driver.exe conv -n 128 -c 160 -H 17 -W 17 -k 160 -y 1 -x 7 -p 0 -q 3 -u 1 -v 1 -l 1 -j 1  -F $FORW
-#./out/conv_driver.exe conv -n 128 -c 1024 -H 14 -W 14 -k 1024 -y 1 -x 1 -p 0 -q 0 -u 1 -v 1 -l 1 -j 1 -g 1 -F $FORW
+./out/conv_driver.exe conv -n 128 -c 128 -H 17 -W 17 -k 128 -y 1 -x 7 -p 0 -q 3 -u 1 -v 1 -l 1 -j 1  -F $FORW
+./out/conv_driver.exe conv -n 128 -c 160 -H 17 -W 17 -k 160 -y 1 -x 7 -p 0 -q 3 -u 1 -v 1 -l 1 -j 1  -F $FORW
+./out/conv_driver.exe conv -n 128 -c 1024 -H 14 -W 14 -k 1024 -y 1 -x 1 -p 0 -q 0 -u 1 -v 1 -l 1 -j 1 -g 1 -F $FORW
 exit 1
 
 #resnext101
