4e0ddee759c9867dc6b22b09eb3e6ca149c293a6
shaojiewang <wsjmessi@163.com>
2021-09-16 14:44:33 +0800

    remove redundant compute in tensor cast function; optimize data load in tensor cast function

--- driver/gpu_tensor_cast/gpu_tensor_cast.cpp
+++ driver/gpu_tensor_cast/gpu_tensor_cast.cpp
@@ -11,20 +11,26 @@ void tensor_cast_fp16_fp32_1d(half* output, float* input, int thread_length, int
     unsigned int tid = threadIdx.x;
     unsigned int bid = blockIdx.x;
 
-    int offset = bid * thread_length * 256 + tid * thread_length;
+    int offset = bid * thread_length * 256;
+    int block_end = offset + thread_length * 256; 
 
-    if(offset + thread_length > total_length){
-        for(int i = offset; i < total_length; i++){
-            in_data = input[i];
+    if(block_end <= total_length)
+    {
+        for(int i = offset; i < block_end; i += 256)
+        {
+            in_data = input[i + tid];
             out_data = (half)(in_data);
-            *(output + i) = out_data;
+            *(output + i + tid) = out_data;
         }
     }
-    else{
-        for(int i = 0; i < thread_length; i++){
-            in_data = input[offset + i];
+    else
+    {
+        for(int i = offset; i < total_length; i += 256)
+        {
+            int index = min(i + tid, total_length - 1);
+            in_data = input[index];
             out_data = (half)(in_data);
-            *(output + offset + i) = out_data;
+            *(output + index) = out_data;
         }
     }
 }
