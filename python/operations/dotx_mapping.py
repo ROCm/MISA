dc83d6959b1e5bdf31d7db7d9c5ba816d63966c3
shaojiewang <wsjmessi@163.com>
2022-01-05 15:30:24 +0800

    fix some bugs of 96x128 tile

--- python/operations/dotx_mapping.py
+++ python/operations/dotx_mapping.py
@@ -227,7 +227,7 @@ ctrl_dotx_mapping_fp16 = [
         # ctrl_dotx_mapping_t(128, 128,  16,  16,   4,   2,   4,   1,   2, v_dot2c_f32_f16),  # extra k pack can be 1, 2
 
         ctrl_dotx_mapping_t(128,  96,   8,   8,   4,   2,   4,   2,   3, v_dot2c_f32_f16),  # extra k pack can be 1, 2, 4
-        ctrl_dotx_mapping_t( 96, 128,   8,   8,   2,   4,   4,   3,   2, v_dot2c_f32_f16),  # extra k pack can be 1, 2, 4
+        ctrl_dotx_mapping_t( 96, 128,   8,   8,   4,   2,   4,   3,   2, v_dot2c_f32_f16),  # extra k pack can be 1, 2, 4
 
         ctrl_dotx_mapping_t(128,  64,   8,   8,   4,   2,   4,   2,   2, v_dot2c_f32_f16),  # extra k pack can be 1, 2, 4
         #ctrl_dotx_mapping_t(128,  64,  16,  16,   4,   2,   4,   1,   1, v_dot2c_f32_f16),  # extra k pack can be 1, 2
