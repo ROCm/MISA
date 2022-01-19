115a23142eb78c14d975a92e991fb5c0a717a9a0
carlushuang <carlus.huang@amd.com>
2022-01-19 16:28:31 +0000

    update smoke test script

--- script/smoke_test.sh
+++ script/smoke_test.sh
@@ -62,11 +62,12 @@ if [ "${ARCH}" != "gfx90a" ] && [ "${ARCH}" != "gfx908" ] && [ "${ARCH}" != "gfx
 fi
 
 export IGEMM_HSACO=out/igemm_${DIR}_gtc_${ARCH}${LAYOUT_HSACO}${PREC_HSACO}.hsaco
+echo IGEMM_HSACO:$IGEMM_HSACO
 export IGEMM_TENSOR_CAST_HSACO=out/igemm_gtc_tensor_cast.hsaco
 export IGEMM_GPU_NAIVE_CONV_HSACO=out/naive_conv.hsaco
 export IGEMM_SCLK_MHZ=1283
 export IGEMM_LOG_FASTEST_CONFIG=1
-export IGEMM_SLEEP_MS=117
+export IGEMM_SLEEP_MS=11
 export PER_PIXEL_CHECK=0
 
 export IGEMM_RAND_INT=1
@@ -88,23 +89,23 @@ fi
 
 EXE=./out/conv_driver.exe
 
-batch_size=( 1, 2 )
+batch_size=( 1 2 )
 image_size=( 14 32 55 )
-channel_size=( 64 32 )
+channel_size=( 64 16 32 )
 group_size=( 1 )
 stride_size=( 1 2 3 )
 dilation_size=( 1 2 3 )
 pad_size=( 0 1 2 3 )
 filter_size=( 7 5 4 3 2 1 )
-tile_x_size=( 0 4 8 11 )
-tile_y_size=( 0 4 8 11 )
+tile_size=( 4 8 11 16 )
+is_tile_list=( 0 1 )
 
-
-for tile_x in "${tile_x_size[@]}"; do
-for tile_y in "${tile_y_size[@]}"; do
-for n  in "${batch_size[@]}"; do
-for c  in "${channel_size[@]}"; do
-for k  in "${channel_size[@]}"; do
+for is_tile in "${is_tile_list[@]}"; do
+#for tile_x in "${tile_x_size[@]}"; do
+#for tile_y in "${tile_y_size[@]}"; do
+#for n  in "${batch_size[@]}"; do
+#for c  in "${channel_size[@]}"; do
+#for k  in "${channel_size[@]}"; do
 for hi in "${image_size[@]}"; do
 for wi in "${image_size[@]}"; do
 for fy in "${filter_size[@]}"; do
@@ -117,14 +118,31 @@ for px in "${pad_size[@]}"; do
 #for dx in "${dilation_size[@]}"; do
 for g  in "${group_size[@]}"; do
 
-isy=$(( $RANDOM % 3 ))
-isx=$(( $RANDOM % 3 ))
-idy=$(( $RANDOM % 3 ))
-idx=$(( $RANDOM % 3 ))
-sy=${stride_size[$isy]}
-sx=${stride_size[$isx]}
-dy=${stride_size[$idy]}
-dx=${stride_size[$idx]}
+i_sy=$(( $RANDOM % 3 ))
+i_sx=$(( $RANDOM % 3 ))
+i_dy=$(( $RANDOM % 3 ))
+i_dx=$(( $RANDOM % 3 ))
+sy=${stride_size[$i_sy]}
+sx=${stride_size[$i_sx]}
+dy=${dilation_size[$i_dy]}
+dx=${dilation_size[$i_dx]}
+
+i_n=$(( $RANDOM % 2 ))
+i_c=$(( $RANDOM % 3 ))
+i_k=$(( $RANDOM % 3 ))
+c=${channel_size[$i_c]}
+k=${channel_size[$i_k]}
+n=${batch_size[$i_n]}
+
+if (( $is_tile == 0 )); then
+tile_x=0
+tile_y=0
+else
+i_tx=$(( $RANDOM % 4 ))
+i_ty=$(( $RANDOM % 4 ))
+tile_x=${tile_size[$i_tx]}
+tile_y=${tile_size[$i_ty]}
+fi
 
 #  (in_size + 2 * pad - dilation * (ksize - 1) - 1) / stride + 1;
 ho=$(( ( $hi + 2 * $py - $dy * ( $fy - 1 ) - 1 ) / $sy + 1  ))
@@ -140,10 +158,18 @@ if (( $ho < $tile_y || $wo < $tile_x )); then
 continue
 fi
 
+i_drop=$(( $RANDOM % 2 ))
+if (( $i_drop == 0 && ( $hi == $wi || $fy == $fx  || $py == $px || $sy == $sx || $dy == $dx || $tile_y == $tile_x ) )) ; then
+continue
+fi
+
+i_drop_2=$(( $RANDOM % 2 ))
+if (( $i_drop_2 == 0 && ( $fy == 4 || $fx == 4  || $fy == 2 || $fx == 2 ) )) ; then
+continue
+fi
+
 echo "${CONV} -n $n -c $c -H $hi -W $wi -k $k -y $fy -x $fx -p $py -q $px -u $sy -v $sx -l $dy -j $dx -g $g -F $FORW ${LAYOUT_ARG} (ho:$ho, wo:$wo, tile_y:$tile_y, tile_x:$tile_x) "
-
 TILE_X=$tile_x TILE_Y=$tile_y $EXE $CONV -n $n -c $c -H $hi -W $wi -k $k -y $fy -x $fx -p $py -q $px -u $sy -v $sx -l $dy -j $dx -g $g -F $FORW ${LAYOUT_ARG} || exit 1
-sleep 0.1
 
 done
 #done
@@ -156,8 +182,9 @@ done
 done
 done
 done
-done
-done
-done
-done
+#done
+#done
+#done
+#done
+#done
 done
